b68dbd9feaaa4861aa461b438721df949a681403
Joel Venzke <joelvenzke@Joels-MacBook-Pro.local>
2013-10-25 20:03:28 -0500

    added uniform cost search

--- venzke_search_project/search_project.py
+++ venzke_search_project/search_project.py
@@ -6,7 +6,6 @@ Description: takes in a list of tiles and finds the order with the lowest 'cost'
 from Tile import Tile
 from State import State
 from Node import Node
-import time
 
 #takes in a file 
 #returns list of tiles from the file
@@ -96,24 +95,61 @@ def depth_first(file_name):
 	best_solution.print_state()
 	print "Nodes Generated: " + str(nodes_generated)
 	
+#takes in a file
+#runs a uniform cost search
+#prints best solution to console
+def uniform_cost(file_name):
+	tile_set = read_file(file_name) #list of tiles
+	start_state = State([],tile_set) #State with no placed tiles
+	root = Node(start_state, 0.0, None) #First Node, no cost, no parent
+	frontier = [root] #list of nodes
+	best_solution = State([],[]) #stores best solution
+	best_cost = float("inf") #set to infinity
+	nodes_generated = len(frontier)
+	keep_running = True
+	nodes_generated = len(frontier)
+
+	#runs while frontier is not empty
+	while keep_running:
+		best_cost_index = None
+		current_best_cost = float("inf")
+		frontier_length = range(len(frontier))
+		for f in frontier_length:
+			if frontier[f].get_cost() < current_best_cost:
+				best_cost_index = f
+				current_best_cost = frontier[f].get_cost()
+				
+		current_node = frontier.pop(best_cost_index) #removes best_cost_index node in frontier, stores in current_node
+		
+		#checks if current_node is a solution 
+		#compares current_node to best_solution
+		#stores new best solution
+		if current_node.get_state().is_goal() and current_node.get_cost() < best_cost:
+			best_cost = current_node.get_cost()
+			best_solution = current_node.get_state()
+
+		#creates child_nodes to search 
+		#adds child_nodes to frontier
+		child_nodes = current_node.expand()
+		nodes_generated = len(child_nodes) + nodes_generated
+		for c_n in child_nodes:
+			frontier.append(c_n)
+			frontier_length = range(len(frontier))
+			keep_running = False 
+		for f in frontier_length:
+			if keep_running == False and frontier[f].get_cost() < best_cost:
+				keep_running = True
+	#prints best solution to console
+	print "HERE IS THE BEST"
+	print best_cost
+	print ""
+	best_solution.print_state()
+	print "Nodes Generated: " + str(nodes_generated)
 
 #User inputs file name
 file_name = raw_input("What file do you want to use? ")
 
-#starts depth first search
-start_time = time.time()
-depth_first(file_name)
-end_time = time.time()
-
-print "Time: " + str(end_time - start_time)
-print " "
-
 #starts breath first search
-start_time = time.time()
-breadth_first(file_name)
-end_time = time.time()
-
-print "Time: " + str(end_time - start_time)
-print " "
-
-
+# depth_first(file_name)
+#breadth_first(file_name)
+uniform_cost(file_name)
