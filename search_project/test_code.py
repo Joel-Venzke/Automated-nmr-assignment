c847c51c357aa51d786cc82ced952e477f2cbe2c
John Emmons <emmons.john@gmail.com>
2014-01-20 14:01:31 -0600

    unittesting

--- search_project/test_code.py
+++ search_project/test_code.py
@@ -3,10 +3,10 @@ import subprocess
 
 class TestSequenceFunctions(unittest.TestCase):
 
-    def testdata_txt(self):
+    def test_testdata_txt(self):
         expected_output = \
 ("""HERE IS THE BEST
-Cost: 1.75179633009
+Cost: 5.67396330086
 
 [54.796, 41.361, 51.848, 19.414]
 [55.396, 30.413, 54.749, 41.328]
@@ -24,10 +24,10 @@ Cost: 1.75179633009
         out = p.communicate()
         self.assertTrue(out == expected_output)
 
-    def mdata_txt(self):
+    def test_mdata_txt(self):
         expected_output = \
 ("""HERE IS THE BEST
-Cost: 1.75179633009
+Cost: 7.26387508926
 
 [54.796, 41.361, 51.848, 19.414]
 [55.396, 30.413, 54.749, 41.328]
@@ -39,9 +39,10 @@ Cost: 1.75179633009
 [66.77, 38.056, 58.701, 29.07]
 [55.393, 17.975, 66.743, 38.1]
 [59.244, 29.006, 55.335, 17.953]
+[-1.0, -1.0, -1.0, -1.0]
 
 """,None)
-        p = subprocess.Popen(["python", "search_project.py", "./Data/testdata.txt"], stdout=subprocess.PIPE)
+        p = subprocess.Popen(["python", "search_project.py", "./Data/mdata.txt"], stdout=subprocess.PIPE)
         out = p.communicate()
         self.assertTrue(out == expected_output)
 
