09a0be30fe92625bca3719ad62466e2b006ab41b
John Emmons <emmons.john@gmail.com>
2014-01-20 13:55:45 -0600

    unittesting

--- search_project/search_project.py
+++ search_project/search_project.py
@@ -7,11 +7,13 @@ Description: takes in a list of tiles and finds the order with the lowest 'cost'
 
 import search_strategies
 import time
+import sys
 
 #User inputs file name
-file_name = raw_input("What file do you want to use? ")
+file_name = sys.argv[1]
 
 start = time.clock()
 search_strategies.uniform_cost(file_name)
 end = time.clock()
-print "Run Time:  " + str(end-start)
+# print "Run Time:  " + str(end-start)
+
