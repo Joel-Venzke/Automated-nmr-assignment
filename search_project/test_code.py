b148cd96328a8bb7c8a49f000461b5db8f2c7e58
DavidMascharka <davidmascharka@gmail.com>
2014-01-30 12:07:00 -0600

    fix to unit testing

--- search_project/test_code.py
+++ search_project/test_code.py
@@ -1,14 +1,12 @@
 import unittest
 import subprocess
+import re
 
 class TestSequenceFunctions(unittest.TestCase):
 
     def test_testdata_txt(self):
         expected_output = \
-("""HERE IS THE BEST
-Cost: 1.75179633009
-
-[54.796, 41.361, 51.848, 19.414]
+"""[54.796, 41.361, 51.848, 19.414]
 [55.396, 30.413, 54.749, 41.328]
 [54.501, 43.445, 55.437, 30.396]
 [60.419, 71.281, 54.235, 43.447]
@@ -18,11 +16,14 @@ Cost: 1.75179633009
 [66.77, 38.056, 58.701, 29.07]
 [55.393, 17.975, 66.743, 38.1]
 [59.244, 29.006, 55.335, 17.953]
-
-""",None)
+"""
         p = subprocess.Popen(["python", "search_project.py", "./Data/testdata.txt"], stdout=subprocess.PIPE)
         out = p.communicate()
-        self.assertTrue(out == expected_output)
+
+        outputList = out[0].split("\n")
+        output = "\n".join(outputList[3:len(outputList) - 1])
+        
+        self.assertTrue(output == expected_output)
 
     def test_mdata_txt(self):
         expected_output = \
