889c9247ddc468e736df76c8cb38cc1bf2b33b19
Joel Venzke <Joel.Venzke@Drake.edu>
2014-02-01 13:10:22 -0600

    fixed error output

--- search_project/search_project.py
+++ search_project/search_project.py
@@ -10,7 +10,7 @@ import time
 import sys
 
 #User inputs file name
-if len(sys.argv):
+if len(sys.argv) < 2:
 	exit("NO INPUT DATA FILE\n\nPlease type: \nshell$ python search_project.py Path/To/Data/File.txt\n")
 
 file_name = sys.argv[1]
