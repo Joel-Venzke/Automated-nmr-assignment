d638f55ff0eb3844ee27d53336d331f4e371ac1a
Rachel <rachel.davis@drake.edu>
2014-02-01 13:34:38 -0600

    adjusting the costs and implementing a char_weight and order_weight

--- search_project/Tile.py
+++ search_project/Tile.py
@@ -11,6 +11,8 @@ class Tile(object):
         self.c = float(c)
         self.d = float(d)
         self.place_holder = place_holder
+        self.char_weight = .5
+        self.order_weight = 1
 
     #returns a list of tile values
     def get_tile(self):
@@ -32,23 +34,24 @@ class Tile(object):
     def get_error(self, char):
         #Missing Data Check:  checks to see if the tile is hard data or a place holder, returns 0 if flexible
         if(self.place_holder == True or self.a and self.b == -1):
-            return .5
+            return self.char_weight
         elif(self.a == -1):
-            return (math.fabs(char[0]-self.a)/self.a)
+
+            return (math.fabs(char[0]-self.a)/self.a*self.char_weight*2)
         elif(self.b == -1):
-            return (math.fabs(char[1]-self.b)/self.b)
+            return (math.fabs(char[1]-self.b)/self.b*self.char_weight*2)
         #all non gly
         else:
-            return (.5*math.fabs(char[0]-self.a)/self.a+.5*math.fabs(char[1]-self.b)/self.b)
+            return (self.char_weight*math.fabs(char[0]-self.a)/self.a+self.char_weight*math.fabs(char[1]-self.b)/self.b)
 
     #takes in next the tile below 
     #returns cost of adding the tile below 
     def compare_above(self, t):
         if(self.place_holder == True or t.get_place_holder() == True or self.a and self.b == -1 or t.c and t.d ==-1 ):
-            return .9
+            return self.order_weight
         elif(self.a == -1 or t.get_c == -1):
-            return math.fabs((self.b-t.get_d()))*2
+            return math.fabs((self.b-t.get_d()))*2*self.order_weight
         elif(self.b == -1 or t.get_d == -1):
-            return math.fabs((self.a-t.get_c()))*2
+            return math.fabs((self.a-t.get_c()))*2*self.order_weight
         else:
-            return  math.fabs((self.a-t.get_c()))+math.fabs((self.b-t.get_d()))
+            return  math.fabs((self.a-t.get_c()))+math.fabs((self.b-t.get_d()))*self.order_weight
