e3cf07c9d75e5731d129f873e22ab596e7bc0ab2
Rachel <rachel.davis@drake.edu>
2014-02-01 13:20:20 -0600

    please don't break

--- search_project/Tile.py
+++ search_project/Tile.py
@@ -31,12 +31,15 @@ class Tile(object):
     #returns the sum
     def get_error(self, char):
         #Missing Data Check:  checks to see if the tile is hard data or a place holder, returns 0 if flexible
-        if(self.place_holder == True):
+        if(self.place_holder == True self.a and self.b == -1):
             return .5
-            
+        elif(self.a == -1):
+            return (math.fabs(char[0]-self.a)/self.a)
+        elif(self.b == -1):
+            return (math.fabs(char[1]-self.b)/self.b)
         #all non gly
         else:
-            return (.5*math.fabs(char[0]-self.a)/self.a+.5*math.fabs(char[0]-self.b)/self.b)
+            return (.5*math.fabs(char[0]-self.a)/self.a+.5*math.fabs(char[1]-self.b)/self.b)
 
     #takes in next the tile below 
     #returns cost of adding the tile below 
