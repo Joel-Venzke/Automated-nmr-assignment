7b0e6f49cf90976f6c5b44525b07ee56ecd45159
DavidMascharka <davidmascharka@gmail.com>
2014-02-16 13:20:05 -0600

    Input files ignore all whitespace

--- search_project/search_strategies.py
+++ search_project/search_strategies.py
@@ -1,6 +1,7 @@
 from Tile import Tile
 from Node import Node
 import sys
+import re
 
 #takes in a file 
 #returns list of tiles from the file
@@ -17,11 +18,12 @@ def read_file(file_name):
 
 		#reads in all lines in the file
 		for line in f:
-
 			#reads each line, splits data at spaces, and adds a new Tile to tile_set_list
 			#file format "a b c d"
-			a, b, c, d = (s for s in line.split(' '))
-			d = d.rstrip()
+			#a, b, c, d = (s for s in line.split(' '))
+			#d = d.rstrip()
+			a, b, c, d = re.findall(r'\b\d+\.\d*.*?\b|\bna\b', line)
+
 			if (a == "na"):
 				a = -1
 			if (b == "na"):
