337a32aeaa09aec80b5a863e3b194ff51898cc00
Joel Venzke <Joel.Venzke@Drake.edu>
2014-02-20 00:44:43 -0600

    fixed typos in data files and update to test_code

--- search_project/test_code.py
+++ search_project/test_code.py
@@ -11,7 +11,7 @@ class TestSequenceFunctions(unittest.TestCase):
 [54.501, 43.445, 55.437, 30.396]
 [60.419, 71.281, 54.235, 43.447]
 [60.125, 29.214, 60.125, 71.214]
-[60.111, 29.007, 60.111, 28.961]
+[60.111, 29.007, 60.111, 29.007]
 [58.758, 29.074, 60.087, 28.961]
 [66.77, 38.056, 58.701, 29.07]
 [55.393, 17.975, 66.743, 38.1]
@@ -25,27 +25,27 @@ class TestSequenceFunctions(unittest.TestCase):
         
         self.assertTrue(output == expected_output)
 
-#     def test_mdata_txt(self):
-#         expected_output = \
-# """[54.796, 41.361, 51.848, 19.414]
-# [55.396, 30.413,  54.749,  41.328]
-# [54.501, 43.445,  55.437,  30.396]
-# [60.419, 71.281,  54.235,  43.447]
-# [60.125, 29.214,  60.125,  71.214]
-# [60.111, 29.007,  60.111,  29.007]
-# [58.758, 29.074,  60.087,  28.961]
-# [66.770, 38.056,  58.701,  29.070]
-# [55.393, 17.975,  66.743,  38.100]
-# [59.244, 29.006,  55.335,  17.953]
-# [58.303, 36.926, -1.0, 29.058]
-# """
-#         p = subprocess.Popen(["python", "search_project.py", "./Data/mdata.txt"], stdout=subprocess.PIPE)
-#         out = p.communicate()
+    def test_mdata_txt(self):
+        expected_output = \
+"""[54.796, 41.361, 51.848, 19.414]
+[55.396, 30.413, 54.749, 41.328]
+[54.501, 43.445, 55.437, 30.396]
+[60.419, 71.281, 54.235, 43.447]
+[60.125, 29.214, 60.125, 71.214]
+[60.111, 29.007, 60.111, 29.007]
+[58.758, 29.074, 60.087, 28.961]
+[66.77, 38.056, 58.701, 29.07]
+[55.393, 17.975, 66.743, 38.1]
+[59.244, 29.006, 55.335, 17.953]
+[58.303, 36.926, -1.0, 29.058]
+"""
+        p = subprocess.Popen(["python", "search_project.py", "./Data/mdata.txt"], stdout=subprocess.PIPE)
+        out = p.communicate()
 
-#         outputList = out[0].split("\n")
-#         output = "\n".join(outputList[3 : len(outputList) - 3])
+        outputList = out[0].split("\n")
+        output = "\n".join(outputList[3 : len(outputList) - 3])
 
-#         self.assertTrue(out == expected_output)
+        self.assertTrue(out == expected_output)
 
     def test_tiny_test_txt(self):
         expected_output = \
@@ -54,7 +54,7 @@ class TestSequenceFunctions(unittest.TestCase):
 [54.501, 43.445, 55.437, 30.396]
 [-1.0, -1.0, -1.0, -1.0]
 [60.125, 29.214, 60.125, 71.214]
-[60.111, 29.007, 60.111, 28.961]
+[60.111, 29.007, 60.111, 29.007]
 [58.758, 29.074, 60.087, 28.961]
 """
 
@@ -73,7 +73,7 @@ class TestSequenceFunctions(unittest.TestCase):
 [54.501, 43.445, 55.437, 30.396]
 [60.419, 71.281, 54.235, 43.447]
 [60.125, 29.214, 60.125, 71.214]
-[60.111, 29.007, 60.111, 28.961]
+[60.111, 29.007, 60.111, 29.007]
 [58.758, 29.074, 60.087, 28.961]
 [66.77, 38.056, 58.701, 29.07]
 [55.393, 17.975, 66.743, 38.1]
@@ -127,28 +127,28 @@ class TestSequenceFunctions(unittest.TestCase):
 
         self.assertTrue(output == expected_output)
 
-#     def test_mdata2_txt(self):
-#         expected_output = \
-# """[54.796, 41.361, 51.848, 19.414]
-# [55.396, 30.413, 54.749, 41.328]
-# [54.501, 43.445, 55.437, 30.396]
-# [-1.0, -1.0, -1.0, -1.0]
-# [60.125, 29.214, 60.125, 71.214]
-# [60.111, 29.007, 60.111, 29.007]
-# [58.758, 29.074, 60.087, 28.961]
-# [66.770, 38.056, 58.701, 29.070]
-# 55.393, 17.975, 66.743, 38.100]
-# [59.244, 29.006, 55.335, 17.953]
-# [-1.0, -1.0, -1.0, -1.0]
-# """
+    def test_mdata2_txt(self):
+        expected_output = \
+"""[54.796, 41.361, 51.848, 19.414]
+[55.396, 30.413, 54.749, 41.328]
+[54.501, 43.445, 55.437, 30.396]
+[-1.0, -1.0, -1.0, -1.0]
+[60.125, 29.214, 60.125, 71.214]
+[60.111, 29.007, 60.111, 29.007]
+[58.758, 29.074, 60.087, 28.961]
+[66.77, 38.056, 58.701, 29.07]
+[55.393, 17.975, 66.743, 38.1]
+[59.244, 29.006, 55.335, 17.953]
+[-1.0, -1.0, -1.0, -1.0]
+"""
 
-#         p = subprocess.Popen(["python", "search_project.py", "./Data/mdata2.txt"], stdout=subprocess.PIPE)
-#         out = p.communicate()
+        p = subprocess.Popen(["python", "search_project.py", "./Data/mdata2.txt"], stdout=subprocess.PIPE)
+        out = p.communicate()
 
-#         outputList = out[0].split("\n")
-#         output = "\n".join(outputList[3 : len(outputList) - 3])
+        outputList = out[0].split("\n")
+        output = "\n".join(outputList[3 : len(outputList) - 3])
 
-#         self.assertTrue(output == expected_output)
+        self.assertTrue(output == expected_output)
 
     def test_tiny_test_4_txt(self):
         expected_output = \
