a20e9e9807f0a29996f2baf321d290761870be00
DavidMascharka <davidmascharka@gmail.com>
2014-03-10 22:06:33 -0500

    Added script to make formatting data for unit test module easier

---
+++ search_project/dataformatscript.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+# DataFormatScript.pl
+# Author: David Mascharka
+# Takes input formatted for reading as input into the assignment program and formats it for use in
+# unit testing. 
+
+use strict;
+
+open (FILE, '>', 'out.txt') or die;
+my $firstline = 1;
+while (<>)
+{
+    if ($firstline) {
+        $firstline = 0;
+        next;
+    }
+
+    chomp $_;
+    $_ = "[$_";
+    while ($_ =~ /(\t+.+$)|([ ]{2,}.+$)/) {
+        s/(\t+)|([ ]{2})/, /g;
+    }
+    
+    while ($_ =~ /0[^\d.]/) {
+        s/0[^\d.]/ /g;
+    }
+
+    while ($_ =~ /na/) {
+        s/na/-1.0/g;
+    }
+    
+    $_ =  "$_]\n";
+    print FILE $_;
+}
+
+close FILE;
