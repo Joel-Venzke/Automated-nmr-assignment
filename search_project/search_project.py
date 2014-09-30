6a5e4abf6a8431cb59e2c8e99821fab8b2f8fc77
Joel Venzke <Joel.Venzke@Drake.edu>
2014-09-30 17:28:37 -0500

    cleaned up start file and updated command line error function

--- search_project/search_project.py
+++ search_project/search_project.py
@@ -10,8 +10,10 @@ import time
 import sys
 
 #User inputs file name
-if len(sys.argv) < 2:
-	exit("NO INPUT DATA FILE\n\nPlease type: \nshell$ python search_project.py Path/To/Data/File.txt NumberOfSearchType\n")
+if len(sys.argv) < 3:
+	exit("NO INPUT DATA FILE\n\nPlease type: \nshell$ python search_project.py Path/To/Data/File.txt NumberOfSearchType\
+		\n\nSearch Type Numbers:\nUniform Cost: 0\nPuzzle Building: 1")
+	
 start = time.clock()
 search_strategies.start_search(sys.argv[1],sys.argv[2])
 end = time.clock()
