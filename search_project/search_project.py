e6daa666b7120087a10dac328cb233e37cef53d3
Joel Venzke <Joel.Venzke@Drake.edu>
2014-09-30 18:16:20 -0500

    command line error output

--- search_project/search_project.py
+++ search_project/search_project.py
@@ -11,9 +11,9 @@ import sys
 
 #User inputs file name
 if len(sys.argv) < 3:
-	exit("NO INPUT DATA FILE\n\nPlease type: \nshell$ python search_project.py Path/To/Data/File.txt NumberOfSearchType\
+	exit("NO INPUT DATA FILE OR MISSING SEARCH TYPE NUMBER\n\nPlease type: \nshell$ python search_project.py Path/To/Data/File.txt NumberOfSearchType\
 		\n\nSearch Type Numbers:\nUniform Cost: 0\nPuzzle Building: 1")
-	
+
 start = time.clock()
 search_strategies.start_search(sys.argv[1],sys.argv[2])
 end = time.clock()
