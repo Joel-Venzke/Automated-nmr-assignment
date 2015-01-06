4f7c6811faf91857bfa113fb03af4c2de12a1f6e
Joel Venzke <Joel.Venzke@Drake.edu>
2015-01-06 15:29:43 -0600

    added new data for retesting

--- search_project/Data_pars/pars.sh
+++ search_project/Data_pars/pars.sh
@@ -1,5 +1,5 @@
 END=61;
 for i in $(seq 1 $END); do
 	echo ../Data/Data_Fall_2014_$i.txt;
-	python pars.py ../Data/Data_Fall_2014.txt $i > ../Data/Data_Fall_2014_$i.txt; 
+	python pars.py ../Data/Data_Fall_2014_fix.txt $i > ../Data/Data_Fall_2014_$i.txt; 
 done
